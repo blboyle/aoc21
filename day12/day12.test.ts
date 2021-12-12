9e6b66de196363a5b957ca3e2357b0dec98dbd54
Brendan Boyle <boyle@Brendans-MacBook-Pro.local>
2021-12-12 14:51:12 -0500

    update test in moving

--- day12/day12.test.ts
+++ day12/day12.test.ts
@@ -1,17 +1,15 @@
-import { input, test1, test2, test3 } from "./fixtures";
-import { Player } from "../src";
+import { input, test1, test2, test3 } from './fixtures';
+import { Player } from '../src';
 
 const player = new Player();
 
-expect(true).toBe(true);
-
-describe("part 1", () => {
+describe('part 1', () => {
   it.each([
     [1, test1, 10],
     [2, test2, 19],
     [3, test3, 226],
     [3, input, 5212],
-  ])("find number of things %i", (_, input, expected) => {
+  ])('find number of things %i', (_, input, expected) => {
     const paths = player.findPaths({ input });
     expect(paths).toBe(expected);
   });
@@ -22,10 +20,13 @@ describe("part 1", () => {
     [3, test3, 3509],
     [4, input, 134862],
   ])(
-    "find number of things %i with longer paths",
+    'find number of things %i with longer paths',
     (number, input, expected) => {
-      const paths = player.findPaths({ input, double: true });
+      const paths = player.findPaths({
+        input,
+        double: true,
+      });
       expect(paths).toBe(expected);
-    }
+    },
   );
 });
