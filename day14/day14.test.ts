d023959c02c0dd8da1f31c2dc6067bbb7f5ea094
Brendan Boyle <boyle@Brendans-MacBook-Pro.local>
2021-12-20 17:37:57 -0400

    skipping day 14

--- day14/day14.test.ts
+++ day14/day14.test.ts
@@ -3,50 +3,52 @@ import { Player } from '../src';
 
 const player = new Player();
 
-describe('part 1', () => {
-  it.skip.each([
-    // [1, 'NCNBCHB'],
-    [2, 'NBCCNBBBCBHCB'],
-    // [3, 'NBBBCNCCNBBNBNBBCHBHHBCHB'],
-    // [
-    //   4,
-    //   'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB',
-    // ],
-  ])('find number of things %i', (steps, expected) => {
-    console.log('---');
-    const { answer } = player.readPolymerization({
-      input: test1,
-      steps,
+describe.skip('day 14 - letter insertion', () => {
+  describe('part 1', () => {
+    it.skip.each([
+      // [1, 'NCNBCHB'],
+      [2, 'NBCCNBBBCBHCB'],
+      // [3, 'NBBBCNCCNBBNBNBBCHBHHBCHB'],
+      // [
+      //   4,
+      //   'NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB',
+      // ],
+    ])('find number of things %i', (steps, expected) => {
+      console.log('---');
+      const { answer } = player.readPolymerization({
+        input: test1,
+        steps,
+      });
+      expect(answer).toBe(expected);
     });
-    expect(answer).toBe(expected);
+
+    it.skip.each([
+      // [1, test1, 1, 1588],
+      // [1, test1, 10, 1588],
+      // [2, input, 10, 3213],
+    ])(
+      'find number of things %i',
+      (_, input, steps, expected) => {
+        const { difference } = player.readPolymerization({
+          input,
+          steps,
+        });
+        expect(difference).toBe(expected);
+      },
+    );
+
+    it.skip.each([
+      // [1, test1, 40, 2188189693529],
+      // [2, input, 10, ],
+    ])(
+      'find number of things %i',
+      (_, input, steps, expected) => {
+        const { difference } = player.readPolymerization({
+          input,
+          steps,
+        });
+        expect(difference).toBe(expected);
+      },
+    );
   });
-
-  it.skip.each([
-    // [1, test1, 1, 1588],
-    // [1, test1, 10, 1588],
-    // [2, input, 10, 3213],
-  ])(
-    'find number of things %i',
-    (_, input, steps, expected) => {
-      const { difference } = player.readPolymerization({
-        input,
-        steps,
-      });
-      expect(difference).toBe(expected);
-    },
-  );
-
-  it.skip.each([
-    // [1, test1, 40, 2188189693529],
-    // [2, input, 10, ],
-  ])(
-    'find number of things %i',
-    (_, input, steps, expected) => {
-      const { difference } = player.readPolymerization({
-        input,
-        steps,
-      });
-      expect(difference).toBe(expected);
-    },
-  );
 });
